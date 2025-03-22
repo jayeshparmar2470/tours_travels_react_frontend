2a5e8def83ce89be094198d669f5bc4a79443bce
JAYESH PARMAR <jayeshparmar2470@gmail.com>
2025-03-23 05:08:49 +0530

    fixed click event in index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ import App from './App';
 import { BrowserRouter , HashRouter as Router} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Intercept link clicks using event delegation
 document.addEventListener('click', (event) => {
-  const { target } = event;
-  if (target.tagName === 'A' && target.getAttribute('href').startsWith('/')) {
+  let target = event.target;
+
+  // Traverse up the DOM tree to find the nearest <a> tag
+  while (target && target.tagName !== 'A') {
+    target = target.parentElement;
+  }
+
+  // If the target is an <a> tag and the href starts with '/'
+  if (target && target.tagName === 'A' && target.getAttribute('href').startsWith('/')) {
     event.preventDefault(); // Prevent default navigation
     const href = target.getAttribute('href'); // Get the href value (e.g., "/about")
-    // const subdirectory = '/reponame/#'; // Add your subdirectory here
-    // window.location.hash = `${subdirectory}${href}`; // Update the hash
-    window.location.hash = `${href}`; // Update the hash
+    window.location.hash = href; // Update the hash
   }
 });
+
 
 root.render(
   <React.StrictMode>
