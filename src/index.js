944fc12022508f62f1b0981fd77ce1e162343c71
JAYESH PARMAR <jayeshparmar2470@gmail.com>
2025-03-23 04:22:08 +0530

    added basename in index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter , HashRouter as Router} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/tours_travels_django_react/#>
+    <Router basename="/tours_travels_django_react/#">
     <App />
     </Router>
   </React.StrictMode>
