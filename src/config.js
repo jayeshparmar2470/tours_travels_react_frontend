cb587a775ac71ed8e074ad2b9ad1f797403f4b5b
JAYESH PARMAR <jayeshparmar2470@gmail.com>
2025-03-24 00:20:27 +0530

    final modification

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
-const API_BASE_URL = 'https://tours-travels-django-backend.onrender.com/api';
+export const API_BASE_URL = 'https://tours-travels-django-backend.onrender.com/api';
 // const API_BASE_URL = 'http://127.0.0.1:8000/api';
 
-const imageUrl = 'https://your-backend-service.onrender.com/media/tours/';
-export default {API_BASE_URL,imageUrl};
+export const imageUrl = 'https://your-backend-service.onrender.com/media/tours/';
+// export default {API_BASE_URL,imageUrl};
